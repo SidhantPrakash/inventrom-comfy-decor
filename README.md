1d54516b1e31c339e904ee954f21dc018293db9e
Sidhant Prakash <sidhant7prakash@gmail.com>
2022-12-13 21:42:57 +0530

    final

---
+++ README.md
@@ -0,0 +1,9 @@
+# Inventrom  
+
+- Complete e-commerce website for decors.
+- products are fetched using axios.
+- Specific products can be searched using keywords, color, price, company.
+- Payment done using stripe.
+- Authentication managed by Auth0.
+
+![Store website](/preview.png)
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="About" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Inventrom is synonymous to well-made furniture that brings alive
+            the aesthetics of your home, giving it an elegant look and a
+            luxurious feel. We aim to make a difference in the way people shop
+            for their furniture and home decor needs. We believe choosing
+            furniture should be easy to acquire and pleasant to look at. This
+            approach helps us stay unique in the industry. All our products
+            provide top-notch quality and aesthetics with the customer reviews
+            backing it up.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
